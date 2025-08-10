0ed4cf5ea293d6df0838d0fab195d0f65fe49740
Дарья Сермяжко <darasermazko@MacBook-Pro-Dara.local>
2025-08-11 02:09:18 +0300

    Сделала блок с сервисом и адаптировала плюс slick slider

--- Js/script.js
+++ Js/script.js
@@ -2,4 +2,15 @@ $(function(){
     $('.mini-menu').click(function(){
         $('nav ul').slideToggle();
     });
+$('.service-slider').slick({
+    arrows: false,
+    dots: true,
+    responsive: [{
+        breakpoint: 600,
+        settings: {
+            dots: false,
+            centerMode: true
+        }
+    }]
+})
 });
