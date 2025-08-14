6da9eaeee83df585199dcbe216c787bda82de66e
Дарья Сермяжко <darasermazko@MacBook-Pro-Dara.local>
2025-08-14 18:15:33 +0300

    Сверстала и адаптировала блок с фото и добавила fancybox

--- Js/script.js
+++ Js/script.js
@@ -1,16 +1,31 @@
-$(function(){
-    $('.mini-menu').click(function(){
+
+$(function() {
+    // Меню
+    $('.mini-menu').click(function() {
         $('nav ul').slideToggle();
-    });
-$('.service-slider').slick({
-    arrows: false,
-    dots: true,
-    responsive: [{
-        breakpoint: 600,
-        settings: {
-            dots: false,
-            centerMode: true
+    }); // <- Добавлена ;
+
+    // Слайдер
+    $('.service-slider').slick({
+        arrows: false,
+        dots: true,
+        responsive: [{
+            breakpoint: 600,
+            settings: {
+                dots: false,
+                centerMode: true
+            }
+        }]
+    }); // <- Добавлена ;
+
+    // Fancybox
+    Fancybox.bind("[data-fancybox='tour']", {
+        Thumbs: false,
+        Toolbar: true,
+        Image: {
+            zoom: true,
+            click: 'zoom',
+            wheel: 'zoom'
         }
-    }]
-})
-});
+    });
+}); // <- Закрывающая скобка для document.ready
